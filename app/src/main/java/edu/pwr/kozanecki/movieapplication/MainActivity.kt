32f7f5f1f7514b0f697b710ceb0c986b95031be3
Jakub Kozanecki <jkozanecki@op.pl>
2023-11-26 13:04:05 +0100

    added navController instead of an Intent

--- app/src/main/java/edu/pwr/kozanecki/movieapplication/MainActivity.kt
+++ app/src/main/java/edu/pwr/kozanecki/movieapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package edu.pwr.kozanecki.movieapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,11 +17,14 @@ import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Divider
@@ -25,11 +32,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import edu.pwr.kozanecki.movieapplication.ui.theme.MovieApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,15 +55,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MovieList(movies = SampleData.moviesList) {movie ->
-                        val intent = Intent(this, MovieDetailsActivity::class.java)
-                        intent.putExtra("index", movie)
-                        startActivity(intent)
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "movieList") {
+                    composable("movieList") {
+                        MovieListScreen(navController = navController)
+                    }
+                    composable("movieDetails/{index}", arguments = listOf(navArgument("index") {type = NavType.IntType})) {
+                            backStackEntry ->
+                        val index = backStackEntry.arguments?.getInt("index")
+                        if(index != null) {
+                            MovieDetailsScreen(index = index)
+                        }
                     }
                 }
             }
@@ -56,11 +77,11 @@ class MainActivity : ComponentActivity() {
 data class Movie(val title: String, val originalTitle: String, val imageSrc: Int, val description: String, val scenes: List<Int>)
 
 @Composable
-fun MovieCard(movie: Movie, index: Int, onCardClick: () -> Unit) {
+fun MovieCard(movie: Movie, index: Int, navController: NavController) {
     Row (
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onCardClick() }) {
+            .clickable { navController.navigate("movieDetails/$index") }) {
         Text(text = String.format("%02d.", index),
             modifier = Modifier.padding(start = 6.dp),
             style = MaterialTheme.typography.titleMedium)
@@ -82,26 +103,146 @@ fun MovieCard(movie: Movie, index: Int, onCardClick: () -> Unit) {
     }
 }
 
-//@Preview
-//@Composable
-//fun PreviewMovieCard() {
-//    MovieCard(movie = Movie("Nietykalni", "Intouchables 2011", R.drawable.nietykalni), index = 1)
-//}
-
 @Composable
-fun MovieList(movies: List<Movie>, onCardClick: (Int) -> Unit) {
+fun MovieList(movies: List<Movie>, navController: NavController) {
     LazyColumn {
         itemsIndexed(movies) {index, movie ->
-            MovieCard(movie = movie, index = index + 1, onCardClick = {onCardClick(index)})
+            MovieCard(movie = movie, index = index, navController = navController)
             Divider(color = Color.Gray, thickness = 1.dp)
         }
     }
 }
 
-//@Preview
-//@Composable
-//fun PreviewMovieList() {
-//    MovieApplicationTheme {
-//        MovieList(movies = SampleData.moviesList)
-//    }
-//}
+@Composable
+fun MovieListScreen(navController: NavController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        MovieList(movies = SampleData.moviesList, navController = navController)
+    }
+}
+
+
+@Composable
+fun MovieDetailsScreen(index: Int) {
+    Log.d("INDEX", index.toString())
+    var showScenes by remember { mutableStateOf(true) }
+    MovieApplicationTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column {
+                MovieDetails(movie = SampleData.moviesList[index])
+                Buttons(
+                    onScenesClick = { showScenes = true },
+                    onActorsClick = { showScenes = false })
+                if (showScenes) DisplayScenes(movieScenes = SampleData.moviesList[index].scenes) else Text(
+                    text = "gowno"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DisplayScenes(movieScenes: List<Int>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3)
+//        contentPadding = PaddingValues(16.dp),
+//        horizontalArrangement = Arrangement.spacedBy(16.dp),
+//       verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(movieScenes.size) { index ->
+            Log.d("DisplayScenes", "Loading image for index: $index, resource: ${movieScenes[index]}")
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .border(1.dp, Color.Black)
+            ) {
+//                Row {
+//                    Text(text = index.toString())
+//                }
+                Image(
+                    painter = painterResource(id = movieScenes[index]),
+                    contentDescription = "Photo $index",
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun MovieDetails(movie: Movie) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(250.dp)
+            .padding(top = 20.dp)
+    ) {
+        Image(
+            painter = painterResource(id = movie.imageSrc),
+            contentDescription = "Photo",
+            modifier = Modifier
+                .size(170.dp)
+                .padding(top = 7.dp)
+        )
+        Text(
+            text = movie.description,
+            modifier = Modifier
+                .padding(end = 5.dp),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+    Divider(color = Color.Gray, thickness = 1.dp)
+}
+
+@Composable
+fun Buttons(onScenesClick: () -> Unit, onActorsClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+        //.padding(bottom = 8.dp),
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .clickable { onScenesClick() }
+                .border(1.dp, Color.Gray)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Text(text = "Scenes",
+                style = MaterialTheme.typography.titleMedium)
+        }
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .clickable { onActorsClick() }
+                .border(1.dp, Color.Gray)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Text(text = "Actors",
+                style = MaterialTheme.typography.titleMedium)
+        }
+    }
+}
+
+
+
+
+
+
+
+
--- app/src/main/java/edu/pwr/kozanecki/movieapplication/MovieDetailsActivity.kt
+++ app/src/main/java/edu/pwr/kozanecki/movieapplication/MovieDetailsActivity.kt
@@ -1,165 +1,149 @@
 package edu.pwr.kozanecki.movieapplication
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import edu.pwr.kozanecki.movieapplication.ui.theme.MovieApplicationTheme
-
-class MovieDetailsActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            var showScenes by remember { mutableStateOf(true) }
-            MovieApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val index =  intent.getIntExtra("index", -1)
-                    Column {
-                        MovieDetails(movie = SampleData.moviesList[index])
-                        Buttons(onScenesClick = { showScenes = true }, onActorsClick = {showScenes = false})
-                        if (showScenes) DisplayScenes(movieScenes = SampleData.moviesList[index].scenes) else Text(text = "gowno")
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun MovieDetails(movie: Movie) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(250.dp)
-                .padding(top = 20.dp)
-        ) {
-            Image(
-                painter = painterResource(id = movie.imageSrc),
-                contentDescription = "Photo",
-                modifier = Modifier
-                    .size(170.dp)
-                    .padding(top = 7.dp)
-            )
-            Text(
-                text = movie.description,
-                modifier = Modifier
-                    .padding(end = 5.dp),
-                style = MaterialTheme.typography.bodyLarge
-            )
-        }
-        Divider(color = Color.Gray, thickness = 1.dp)
-}
-
-@Composable
-fun Buttons(onScenesClick: () -> Unit, onActorsClick: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            //.padding(bottom = 8.dp),
-    ) {
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .clickable { onScenesClick() }
-                .border(1.dp, Color.Gray)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ) {
-            Text(text = "Scenes",
-                style = MaterialTheme.typography.titleMedium)
-        }
-
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .clickable { onActorsClick() }
-                .border(1.dp, Color.Gray)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ) {
-            Text(text = "Actors",
-                style = MaterialTheme.typography.titleMedium)
-        }
-    }
-}
-
-@Composable
-fun DisplayScenes(movieScenes: List<Int>) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(3),
-//        contentPadding = PaddingValues(16.dp),
-//        horizontalArrangement = Arrangement.spacedBy(16.dp),
-//       verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        items(movieScenes.size) { index ->
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .border(1.dp, Color.Black)
-            ) {
-                Image(
-                    painter = painterResource(id = movieScenes[index]),
-                    contentDescription = "Photo $index",
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
-
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String?, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieApplicationTheme {
-        Greeting("Android")
-    }
-}
+//
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.compose.foundation.Image
+//import androidx.compose.foundation.border
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Box
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.PaddingValues
+//import androidx.compose.foundation.layout.Row
+//import androidx.compose.foundation.layout.fillMaxHeight
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxWidth
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.lazy.grid.GridCells
+//import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+//import androidx.compose.material3.Button
+//import androidx.compose.material3.Divider
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.material3.Surface
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.mutableStateOf
+//import androidx.compose.runtime.remember
+//import androidx.compose.runtime.setValue
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.res.painterResource
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import edu.pwr.kozanecki.movieapplication.ui.theme.MovieApplicationTheme
+//
+//class MovieDetailsActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            var showScenes by remember { mutableStateOf(true) }
+//            MovieApplicationTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    val index =  intent.getIntExtra("index", -1)
+//                    Column {
+//                        MovieDetails(movie = SampleData.moviesList[index])
+//                        Buttons(onScenesClick = { showScenes = true }, onActorsClick = {showScenes = false})
+//                        if (showScenes) DisplayScenes(movieScenes = SampleData.moviesList[index].scenes) else Text(text = "gowno")
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun MovieDetails(movie: Movie) {
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .height(250.dp)
+//                .padding(top = 20.dp)
+//        ) {
+//            Image(
+//                painter = painterResource(id = movie.imageSrc),
+//                contentDescription = "Photo",
+//                modifier = Modifier
+//                    .size(170.dp)
+//                    .padding(top = 7.dp)
+//            )
+//            Text(
+//                text = movie.description,
+//                modifier = Modifier
+//                    .padding(end = 5.dp),
+//                style = MaterialTheme.typography.bodyLarge
+//            )
+//        }
+//        Divider(color = Color.Gray, thickness = 1.dp)
+//}
+//
+//@Composable
+//fun Buttons(onScenesClick: () -> Unit, onActorsClick: () -> Unit) {
+//    Row(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            //.padding(bottom = 8.dp),
+//    ) {
+//        Column(
+//            modifier = Modifier
+//                .weight(1f)
+//                .clickable { onScenesClick() }
+//                .border(1.dp, Color.Gray)
+//                .padding(16.dp),
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally
+//
+//        ) {
+//            Text(text = "Scenes",
+//                style = MaterialTheme.typography.titleMedium)
+//        }
+//
+//        Column(
+//            modifier = Modifier
+//                .weight(1f)
+//                .clickable { onActorsClick() }
+//                .border(1.dp, Color.Gray)
+//                .padding(16.dp),
+//            verticalArrangement = Arrangement.Center,
+//            horizontalAlignment = Alignment.CenterHorizontally
+//
+//        ) {
+//            Text(text = "Actors",
+//                style = MaterialTheme.typography.titleMedium)
+//        }
+//    }
+//}
+//
+//@Composable
+//fun DisplayScenes(movieScenes: List<Int>) {
+//    LazyVerticalGrid(
+//        columns = GridCells.Fixed(3),
+////        contentPadding = PaddingValues(16.dp),
+////        horizontalArrangement = Arrangement.spacedBy(16.dp),
+////       verticalArrangement = Arrangement.spacedBy(16.dp)
+//    ) {
+//        items(movieScenes.size) { index ->
+//            Box(
+//                contentAlignment = Alignment.Center,
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .border(1.dp, Color.Black)
+//            ) {
+//                Image(
+//                    painter = painterResource(id = movieScenes[index]),
+//                    contentDescription = "Photo $index",
+//                    modifier = Modifier.fillMaxSize()
+//                )
+//            }
+//
+//        }
+//    }
+//}
